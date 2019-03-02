46aace840f389c51e9424a0c55fe9d14e7c60d1b
garghimanshu13 <48146125+garghimanshu13@users.noreply.github.com>
2019-03-02 17:10:07 +0530

    ad html

--- HealthCare/HealthCare.Web/Controllers/HomeController.cs
+++ HealthCare/HealthCare.Web/Controllers/HomeController.cs
@@ -8,6 +8,12 @@ namespace HealthCare.Web.Controllers
 {
     public class HomeController : Controller
     {
+
+        public ActionResult Login()
+        {
+            return View();
+        }
+
         public ActionResult Index()
         {
             return View();
